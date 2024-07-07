c12e0affa4feec802fa459511921da9ee55248d6
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2024-07-07 14:40:44 +0200

    updated PersonsModelMigrator

--- edelta.example.migration.personlist.migrator/src/edelta/example/migration/personlist/migrator/PersonsModelMigrator.java
+++ edelta.example.migration.personlist.migrator/src/edelta/example/migration/personlist/migrator/PersonsModelMigrator.java
@@ -2,6 +2,7 @@ package edelta.example.migration.personlist.migrator;
 
 import edelta.FromV1ToV2;
 import edelta.FromV2ToV3;
+import edelta.FromV3ToV4;
 import edelta.lib.EdeltaVersionMigrator;
 import persons.PersonsPackage;
 
@@ -14,6 +15,7 @@ public class PersonsModelMigrator {
 		// register the Edelta migration code
 		edeltaMigrator.registerMigration(FromV2ToV3::new);
 		edeltaMigrator.registerMigration(FromV1ToV2::new);
+		edeltaMigrator.registerMigration(FromV3ToV4::new);
 		// load the models to check and migrate
 		edeltaMigrator.addModelFileExtension(".persons");
 		edeltaMigrator.loadModelsFrom(modelPath);
