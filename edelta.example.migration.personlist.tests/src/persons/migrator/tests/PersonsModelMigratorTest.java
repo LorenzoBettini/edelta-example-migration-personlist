dccaa6dfc7f60d4b1e5af2f92d2aab1ec2d853a4
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2024-07-07 15:56:26 +0200

    test case for PersonsModelMigrator

---
+++ edelta.example.migration.personlist.tests/src/persons/migrator/tests/PersonsModelMigratorTest.java
@@ -0,0 +1,38 @@
+package persons.migrator.tests;
+
+import java.io.IOException;
+import java.util.Collection;
+
+import org.eclipse.emf.ecore.resource.Resource;
+import org.junit.Before;
+import org.junit.Test;
+
+import edelta.example.migration.personlist.migrator.PersonsModelMigrator;
+import edelta.testutils.EdeltaTestUtils;
+
+public class PersonsModelMigratorTest {
+
+	private static final String OUTPUT = "output/";
+	private static final String INPUTS = "inputs/";
+	private static final String EXPECTATIONS = "expectations/";
+	private static final String TEST_MODEL = "My.persons";
+
+	private PersonsModelMigrator personsModelMigrator;
+
+	@Before
+	public void setup() throws IOException {
+		EdeltaTestUtils.cleanDirectoryRecursive(OUTPUT);
+		personsModelMigrator = new PersonsModelMigrator();
+	}
+
+	@Test
+	public void testFromV1() throws Exception {
+		EdeltaTestUtils.copyDirectory(INPUTS + "v1", OUTPUT);
+		Collection<Resource> migrated = personsModelMigrator.execute(OUTPUT);
+		EdeltaTestUtils.assertFilesAreEquals(
+				EXPECTATIONS + TEST_MODEL,
+				OUTPUT + TEST_MODEL);
+		EdeltaTestUtils.assertResourcesAreValid(migrated);
+	}
+
+}
